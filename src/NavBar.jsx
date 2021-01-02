0fbb3f109dc056988bd5a6513f33b0ab13986bab
barrettmckinney <54785201+barrettmckinney@users.noreply.github.com>
2021-01-02 11:46:05 -0500

    Add files via upload

---
+++ src/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import Logo from "./assets/images/logo.png";
+import "./assets/styles/navbar.css";
+
+const NavItem = (props) => {
+  const { to, children } = props;
+  return (
+    <NavLink className="nav-item" to={to}>
+      {children}
+    </NavLink>
+  );
+};
+
+const NavBar = (props) => (
+  <nav className="nav-bar">
+    <NavItem to="/">Home</NavItem>
+    {/* <NavItem to="/team">Our Team</NavItem> */}
+    <NavItem to="/console">Console</NavItem>
+    <div className="nav-bar-logo-container">
+      <NavLink to="/console">
+        <img className="nav-bar-logo" src={Logo} alt="logo" />
+      </NavLink>
+    </div>
+  </nav>
+);
+
+export default NavBar;
