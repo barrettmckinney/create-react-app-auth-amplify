17d2ec8103e12cc44b128fd865fb22275adfced9
barrettmckinney <54785201+barrettmckinney@users.noreply.github.com>
2021-01-03 12:50:53 -0500

    no more nav bar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 
 import { BrowserRouter as Router, useLocation } from "react-router-dom";
-import NavBar from "./NavBar";
 
 Amplify.configure(aws_exports);
 
