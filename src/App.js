db6a6378317f6df1f7cf2be74a479e03cb65608e
barrettmckinney <54785201+barrettmckinney@users.noreply.github.com>
2021-01-03 14:46:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             Welcome! Manage your virtual desktops
               <div>
                <a href="https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/create/review?templateURL=https://awsfirework.s3-us-west-2.amazonaws.com/CreateNewWorkSpace.yaml&stackName=New-Workspace">
-                  <img alt="Create New Workspaces" src="src/images/workspaces.png"/>
+                  <img alt="Create New Workspaces" src="https://images-na.ssl-images-amazon.com/images/I/512NmkgumLL.png"/>
               </a>
             </div>
           </p>
