9c809a125dca994767f97f96b708ada60ab09367
barrettmckinney <54785201+barrettmckinney@users.noreply.github.com>
2021-01-04 13:08:17 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
         <header className="App-header">
           <p>
               <div>
-               <img src = "https://awsfirework.s3-us-west-2.amazonaws.com/fireworkmockup.jpg"/>
+               <img src = "https://awsfirework.s3-us-west-2.amazonaws.com/fireworkmockup.jpg" width = 100% height = 100%/>
             </div>
           </p>
         </header>
